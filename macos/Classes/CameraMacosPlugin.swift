cab11ccbbbd5ce82a5660f561f37ebc1d17b0173
johannesvuorinen <johannes@frogmindgames.com>
2023-06-27 07:51:57 +0300

    Update CameraMacosPlugin.swift

--- macos/Classes/CameraMacosPlugin.swift
+++ macos/Classes/CameraMacosPlugin.swift
@@ -993,7 +993,7 @@ public class CameraMacosPlugin: NSObject, FlutterPlugin, FlutterTexture, AVCaptu
         if !isBufferAudio {
             latestBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
             DispatchQueue.main.async {
-                registry.textureFrameAvailable(textureId)
+                self.registry.textureFrameAvailable(textureId)
             }
         }
         
